a98664497a7da7564a35ca218501e3030447aedd
Richard <79211807+Richard-01@users.noreply.github.com>
2024-03-13 22:37:29 -0500

    update files - error persiste

--- conexion.js
+++ conexion.js
@@ -1,20 +1,23 @@
 const mongoose = require('mongoose');
+const MONGODB_URI = "mongodb+srv://richardmontoyaa1:Nn2gzy3EeLIyvcTq@cluster0.aqp1drz.mongodb.net/";
 
 let conn = null;
-const MONGODB_URI ="mongodb+srv://richardmontoyaa1:Nn2gzy3EeLIyvcTq@cluster0.aqp1drz.mongodb.net/";
 
 const connector = async () => {
-  if (conn === null) {
-    conn = await mongoose.createConnection(MONGODB_URI, {
-      serverSelectionTimeoutMS: 10000,
-      socketTimeoutMS: 45000,
-    });
+  try {
+    if (conn === null) {
+      conn = await mongoose.createConnection(MONGODB_URI, {
+        serverSelectionTimeoutMS: 10000,
+        socketTimeoutMS: 45000,
+      });
+    }
+    return conn;
+
+  } catch (error) {
+    throw new Error(`Error al conectar a la base de datos: ${error.message}`);
   }
 
-  return conn;
 };
 
 
-module.exports = {
-    connector,
-};
+module.exports = connector;
