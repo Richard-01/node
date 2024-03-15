a5e3341ee8903f61ac6b38deea111f55c8c5434b
Richard Montoya <79211807+Richard-01@users.noreply.github.com>
2024-03-15 07:26:03 -0500

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,6 +6,8 @@ const router = express.Router();
 router
     .get('/', controllers.getHome)
     .get('/users', controllers.getAllUsers)
+    .get('/users/:id', controllers.getUserById)
+    .post('/users/createUser', controllers.createUser)
 
 module.exports = {
     router,
